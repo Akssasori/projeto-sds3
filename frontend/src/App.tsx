26865fc6f06c9a4f9ca80bda426700d1eec440e1
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2021-05-06 09:42:08 -0300

    funcionando aparecendo DevSuperior

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
+import NavBar from "components/NavBar";
+
+
 
 function App() {
   return (
     <div>
       <h1 className="text-primary">Olá mundo!</h1>
+      <NavBar />
+      <NavBar />
+      <NavBar />
     </div>
     
   );
